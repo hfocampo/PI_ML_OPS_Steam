e4cd020f2d6b9e6e3bf22f98de808c7d9eb2aff3
hfocampo <hector.ocampog@gmail.com>
2024-02-06 07:55:58 -0500

    v.11

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from sklearn.metrics.pairwise        import cosine_similarity
 from sklearn.metrics.pairwise        import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-df = pd.read_csv('dataset_finales/DataSet_Final.csv')
-df_games = pd.read_csv('dataset_finales/steam_games_organizados.csv')
-
 
 app=FastAPI()#debug=True)
+
+df = pd.read_csv('/dataset_finales/DataSet_Final.csv')
+df_games = pd.read_csv('/dataset_finales/steam_games_organizados.csv')
 
 @app.get('/')
 def message():
